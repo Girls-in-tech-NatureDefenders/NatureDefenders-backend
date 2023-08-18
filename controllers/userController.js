37ed1ac8c7860547362b97aef006d641b3f4cac8
joy <joyagbo49@gmail.com>
2023-08-18 03:32:28 +0100

    Made corrections

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,35 +3,11 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 const { handleErrors } = require("../errorResponse/handleErrors");
 const secret = process.env.MY_SECRET
-// const tokenAge = 100 * 60 * 60 * 24; // 24 hour
-
-//handle errors
-// const handleErrors = (err)=>{
-//     console.log(err.message, err.code)
-//     let errors = { fullName:'', email:'', password:'', countryOfResidence:'', role:''}
-    
-//     //incorrect email and password error handler
-//     if(err.message === 'incorrect email'){
-//         errors.email = 'that email is not registered'
-//     }
-//     if(err.message === 'incorrect password'){
-//         errors.password = 'that password is incorrect'
-//     }
 
 
-//     //duplicate error code
-//     if(err.code ===11000){
-//         errors = 'user already exist'
-//         return errors
-//     }
-//     //validation errors
-//     if (err.message.includes('User validation failed')){
-//         (Object.values(err.errors)).forEach(({properties}) =>{
-//         errors[properties.path] = properties.message
-//         })
-//     }
-//     return errors
-// }
+
+
+
 const maxAge = 3*24*60*60
 const createToken = (id)=>{
     return jwt.sign({id}, secret,{
@@ -70,6 +46,7 @@ module.exports.register_post = async(req, res)=>{
     res.status(400).json({errors})
     }
 }
+
 
 module.exports.login_post = async(req, res)=>{
   const {email, password} = req.body
